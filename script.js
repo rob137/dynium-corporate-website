6b8b96c5039c3987a3079e6c92beecbcdb92345a
rob <robertaxelkirby@gmail.com>
2019-09-03 14:32:55 +0100

    Tidy nav menu class names

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const toggleMenu = () => {
-  navContent = document.getElementsByClassName('header_nav')[0].innerHTML;
-  if (navContent) {
-    document.getElementsByClassName('header_nav')[0].innerHTML = ``;
+  navMenu = document.getElementsByClassName('header_inner_nav_menu')[0];
+  if (navMenu.innerHTML) {
+    navMenu.innerHTML = ``;
   } else {
-    document.getElementsByClassName('header_nav')[0].innerHTML = `
+    navMenu.innerHTML = `
       <ul class="header_inner_menu">
         <li><a>Product</a></li>
         <li><a>Careers</a></li>
@@ -12,6 +12,6 @@ const toggleMenu = () => {
   }
 };
 
-const navButton = document.getElementsByClassName('header_inner_menuBtn')[0];
+const navButton = document.getElementsByClassName('header_inner_nav_menuBtn')[0];
 
 navButton.addEventListener('click', toggleMenu);
